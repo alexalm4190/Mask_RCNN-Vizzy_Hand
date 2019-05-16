c9efd1d5545dfe6c546f1289db9fbaa015ba8c0d
alexalm4190 <alexalm4190@hotmail.com>
2019-05-16 16:23:34 +0100

    added a new code to visualize masks on the gt images

--- utils/annotate_masks.py
+++ utils/annotate_masks.py
@@ -37,9 +37,10 @@ def click_and_crop(event, x, y, flags, param):
 imageNames = os.listdir(os.path.sep.join([arguments.imagesDir, "images"]))
 for filename in imageNames:
     image = cv2.imread(os.path.sep.join([arguments.imagesDir, "images", filename]))
-    image = cv2.resize(image,(1000, 600), interpolation=cv2.INTER_LINEAR)
+    image = cv2.resize(image,(1200, 680), interpolation=cv2.INTER_LINEAR)
     mask = np.zeros((image.shape[0], image.shape[1]))
     mask[:] = 255
+    initImage = image.copy()
 
     cv2.namedWindow("image")
     cv2.setMouseCallback("image", click_and_crop)
@@ -51,15 +52,32 @@ for filename in imageNames:
 
         # if the 'q' key is pressed, break from the loop
         if key == ord("q"):
-            break        
+            fill_row_coords, fill_col_coords = draw.polygon(np.array(rowCoords), np.array(columnCoords), image.shape)
+            mask[fill_row_coords, fill_col_coords] = 0
+            rowCoords = []
+            columnCoords = []
+            #mask[:] = tuple((255, 255, 255))
+            #cv2.fillConvexPoly(mask, np.array(refPt), tuple((0, 0, 0)))
+            cv2.imwrite(os.path.sep.join([arguments.imagesDir, "mask", filename]), mask)
+            cv2.imwrite(os.path.sep.join([arguments.imagesDir, "images", filename]), initImage)
+            print("image " + filename + " saved")
+            break     
+        # if the 'r' key is pressed, reset the image   
+        elif key == ord("r"):
+            image = initImage.copy()
+            rowCoords = []
+            columnCoords = [] 
+        # if the 'p' key is pressed, draw the polygn 
+        elif key == ord("p"):
+            fill_row_coords, fill_col_coords = draw.polygon(np.array(rowCoords), np.array(columnCoords), image.shape)
+            mask[fill_row_coords, fill_col_coords] = 0
+            rowCoords = []
+            columnCoords = []
+            print("polygn drawn... draw other polygns or press 'q' to quit this image")    
+        # if the 'e' key is pressed, pass to the next image (without saving anything) 
+        elif key == ord("e"):
+            rowCoords = []
+            columnCoords = []
+            break 
 
     #refPt.append(refPt[0])
-    
-    #mask[:] = tuple((255, 255, 255))
-    #cv2.fillConvexPoly(mask, np.array(refPt), tuple((0, 0, 0)))
-    fill_row_coords, fill_col_coords = draw.polygon(np.array(rowCoords), np.array(columnCoords), image.shape)
-    mask[fill_row_coords, fill_col_coords] = 0
-    cv2.imwrite(os.path.sep.join([arguments.imagesDir, "mask", filename]), mask)
-    
-    rowCoords = []
-    columnCoords = []
