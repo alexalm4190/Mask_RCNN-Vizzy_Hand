c060270fad4bb714db30ae22d34023c11d6b909f
alexalm4190 <alexalm4190@hotmail.com>
2019-06-10 16:54:40 +0100

    saves images during the visualization of the results

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -84,7 +84,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
+                      colors=None, captions=None, save_path=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -164,6 +164,9 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    if save_path != None:
+        fig = ax.get_figure()
+        fig.savefig(save_path)
     if auto_show:
         plt.show()
 
