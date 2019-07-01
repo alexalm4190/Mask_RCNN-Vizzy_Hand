bbc4400e7efb8a665a03b50fdde44745219fd628
alexalm4190 <alexalm4190@hotmail.com>
2019-07-01 16:31:53 +0100

    new code to create more training images

---
+++ utils/data_augmentation.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+class Augmentation():
+
+    def __init__(self, dataset=None):
+
+        self.dataset = dataset
+
+    def add_hand_to_background(self, background, mask, image, pos_index):
+
+        background[mask==pos_index] = image[mask==pos_index]
+
+        return background
+
+    def create_negative_sample(self):
+        
+        mask = cv2.imread("/home/alexandre/train_images/mask/FrameBuffer_0000.png", 0)
+        image = cv2.imread("/home/alexandre/train_images/images/FrameBuffer_0000.png")
+        background = cv2.imread("/home/alexandre/teste.jpeg")
+        background = cv2.resize(background, (1280, 960))
+
+        pos_index = 0
+        background = self.add_hand_to_background(background, mask, image, pos_index)
+        return background
+
+augmentation = Augmentation()
+image = augmentation.create_negative_sample()
+
+cv2.imwrite("/home/alexandre/test_result.png", image)
