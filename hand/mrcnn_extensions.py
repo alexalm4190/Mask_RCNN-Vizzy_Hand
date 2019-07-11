55986694c0af16c0e1a5f3245f882c6c777f4108
alexalm4190 <alexalm4190@hotmail.com>
2019-07-11 09:35:29 +0100

    changed num steps

--- hand/mrcnn_extensions.py
+++ hand/mrcnn_extensions.py
@@ -28,9 +28,9 @@ class HandConfig(Config):
 
 	TRAIN_ROIS_PER_IMAGE = 16
 
-	STEPS_PER_EPOCH = 1554
+	STEPS_PER_EPOCH = 952
 
-	VALIDATION_STEPS = 388
+	VALIDATION_STEPS = 190
 
 class HandDataset(utils.Dataset):
     
